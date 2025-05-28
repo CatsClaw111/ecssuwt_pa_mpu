6e58de9e95188118325625a260464d67528c2a4d
NikitaTomash <natifzy@gmail.com>
2025-05-28 14:27:20 +0300

    models,urls,views

--- OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/polytechApp/settings.py
+++ OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/polytechApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,8 @@ WSGI_APPLICATION = 'polytechApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postresql',
-        'NAME': BASE_DIR / 'db.postresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
