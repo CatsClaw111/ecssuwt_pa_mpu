ae9b65b2bc834d8fdeb6bd4fbe551ec517d97247
NikitaTomash <natifzy@gmail.com>
2025-05-28 16:00:49 +0300

    3 работающие страницы, в main. base как шаблон для шаблонов в polytechApp/templates. Админ может создавать категории,создавать проекты, добавлять фотографии отдельно. Папка main/urls для приложения main связана с основным urls.

--- OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/main/urls.py
+++ OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('project/<int:project_id>/', views.project, name='project'),
 ]
--- OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/main/views.py
+++ OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project, Category
 
 def index(request):
-    render(request,'main/index.html')
+    projects = Project.objects.order_by('?')[:3]  # Выбираем 3 случайных проекта
+    return render(request, 'main/index.html', {'projects': projects})
 
 def catalog(request):
-    render(request,'main/catalog.html')
+    projects = Project.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'main/catalog.html', {'projects': projects, 'categories': categories})
 
+def project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    return render(request, 'main/project.html', {'project': project})
