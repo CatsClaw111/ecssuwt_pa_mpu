20e27b84b880a7f61891ab5d4c85f88096969cec
NikitaTomash <natifzy@gmail.com>
2025-05-30 19:21:44 +0300

    new branch

--- OneDrive/Рабочий стол/VSCode/Kursach/polytechApp/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('catalog/', views.catalog, name='catalog'),
-    path('project/<int:project_id>/', views.project, name='project'),
-]
